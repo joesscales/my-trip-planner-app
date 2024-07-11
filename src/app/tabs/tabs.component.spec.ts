265cc7fab7a18a056cff317bb1a419db182c9c9a
joesscales <89870266+joesscales@users.noreply.github.com>
2024-07-11 09:35:45 +0100

    test: TabsComponent test for checking buttons and routes

--- src/app/tabs/tabs.component.spec.ts
+++ src/app/tabs/tabs.component.spec.ts
@@ -1,22 +1,69 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { Location } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
+import { routes } from '../app.routes';
 import { TabsComponent } from './tabs.component';
 
-describe('TabsComponent', () => {
-  let component: TabsComponent;
+describe('Tabs Component', () => {
+
+  let harness: RouterTestingHarness;
   let fixture: ComponentFixture<TabsComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [TabsComponent],
+  let component: TabsComponent;
+  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot()],
+      providers: [
+        provideRouter(routes)
+         ]
     }).compileComponents();
+     harness = await RouterTestingHarness.create();
+     fixture = TestBed.createComponent(TabsComponent);
+     component = fixture.componentInstance;
+     fixture.detectChanges();
 
-    fixture = TestBed.createComponent(TabsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+   
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have three tab buttons', () => {
+    const tabButtons = fixture.debugElement.queryAll(By.css('ion-tab-button'));
+    expect(tabButtons.length).toBe(3);
+  });
+
+
+  it('should navigate to correct routes when tabs are clicked', async () => {
+    
+    const location = TestBed.inject(Location);
+
+    const tabBar = fixture.debugElement.query(By.css('ion-tab-bar'));
+    const tabButtons = tabBar.queryAll(By.css('ion-tab-button'));
+
+    const homeTab = tabButtons.find((tab: { nativeElement: { getAttribute: (arg0: string) => string; }; }) => tab.nativeElement.getAttribute('tab') === 'home');
+    const exploreTab = tabButtons.find((tab: { nativeElement: { getAttribute: (arg0: string) => string; }; }) => tab.nativeElement.getAttribute('tab') === 'explore');
+    const tripTab = tabButtons.find((tab: { nativeElement: { getAttribute: (arg0: string) => string; }; }) => tab.nativeElement.getAttribute('tab') === 'trip');
+
+    if (!homeTab || !exploreTab || !tripTab) {
+      throw new Error('One or more tab buttons not found');
+    }
+
+     homeTab.nativeElement.click();
+      await harness.navigateByUrl('/home');
+      expect(location.path()).toBe('/home');
+    
+
+    exploreTab.nativeElement.click();
+    await harness.navigateByUrl('/explore');
+    expect(location.path()).toBe('/explore');
+
+    tripTab.nativeElement.click();
+    await harness.navigateByUrl('/trip');
+    expect(location.path()).toBe('/trip');
   });
 });
